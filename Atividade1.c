fb3ff1ac88cc1f16cccecfdf8fbf2808c7e44ebe
shaolyn-fantastic <joaofegao3@gmail.com>
2021-10-24 13:27:14 -0300

    o metodo de amarzenar os decimais Vai ter que ser por string

--- Atividade1.c
+++ Atividade1.c
@@ -9,6 +9,18 @@ char RetornaOpcao(FILE* arq){
   return  fgetc(arq);
 }
 
+int QuantidadeLinhasArquivo(FILE* arq){
+  int contagem = 1;
+      int caractere;
+      do {
+          caractere = fgetc(arq);
+          if(caractere == '\n') {
+              contagem++;
+          }
+      } while(caractere != EOF);
+
+return contagem;
+}
 char EntradaDecimais(FILE *arq){
 
 }
@@ -20,7 +32,7 @@ char EntradaDecimais(FILE *arq){
    }PilhaEstatica;
 
 FILE* arq;
-arq = fopen("entrada01.txt","r");
+arq = fopen("entrada04.txt","r");
 if(arq == NULL ){
   printf("Arquivo Vazio\n");
 }
@@ -32,35 +44,23 @@ if(arq == NULL ){
   }else{
 
   }
+char caractere;
+int vetordecimais[6];
+for(int i=0; i<6; i++){
+    caractere = fgetc(arq);
+    vetordecimais[i] = caractere -'0';//converte char em int
+    if(i%2 != 0){
+    printf("vetor | %d |",vetordecimais[i]);
+}
+  }
+
+
+
+
   /*PilhaEstatica p;
   iniciaPilhaEstatica(&p);*/
 
 //adquirir dados do TEXTO
-  char Linha[10];
-  char *result;
-char letra ='\n';
-  int dec[4];
-  int contador = 0;
-
-  for(int i=0; i<4; i++){
-	// Lê uma linha (inclusive com o '\n')
-      result = fgets(Linha, 10, arq);  // o 'fgets' lê até 99 caracteres ou até o '\n'
-      if (result)  // Se foi possível ler
-      dec[i] = (int)Linha;
-      printf("Linha %d : %s\n",i,dec[i]);
-    }
-
-    for(int i=0; i<4; i++){
-  	// Lê uma linha (inclusive com o '\n')
-        result = fgets(Linha, 10, arq);  // o 'fgets' lê até 99 caracteres ou até o '\n'
-        if (result){
-          if(Linha == letra){
-          contador++;}
-        }
-
-      }
-      printf("contador %d\n",contador);
-
-
+fclose(arq);
   return 0;
 }
