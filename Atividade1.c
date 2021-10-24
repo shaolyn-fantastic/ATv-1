e91593356fab970f95ce2f100fe1c4d0ded1859e
shaolyn-fantastic <joaofegao3@gmail.com>
2021-10-24 12:19:07 -0300

    Erro na captação por string

--- Atividade1.c
+++ Atividade1.c
@@ -38,17 +38,28 @@ if(arq == NULL ){
 //adquirir dados do TEXTO
   char Linha[10];
   char *result;
-  int i;
-  i = 1;
-  while (!feof(arq))
-  {
+char letra ='\n';
+  int dec[4];
+  int contador = 0;
+
+  for(int i=0; i<4; i++){
 	// Lê uma linha (inclusive com o '\n')
       result = fgets(Linha, 10, arq);  // o 'fgets' lê até 99 caracteres ou até o '\n'
       if (result)  // Se foi possível ler
-	  printf("Linha %d : %s",i,Linha);
-    for(int i=0; i<)
-      i++;
-  }
+      dec[i] = (int)Linha;
+      printf("Linha %d : %s\n",i,dec[i]);
+    }
+
+    for(int i=0; i<4; i++){
+  	// Lê uma linha (inclusive com o '\n')
+        result = fgets(Linha, 10, arq);  // o 'fgets' lê até 99 caracteres ou até o '\n'
+        if (result){
+          if(Linha == letra){
+          contador++;}
+        }
+
+      }
+      printf("contador %d\n",contador);
 
 
   return 0;
